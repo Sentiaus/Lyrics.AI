f0f0aa725d3a22a8e61694162ec910e76758525e
Victor Fawole <victorfawole@gmail.com>
2024-08-23 00:44:44 -0400

    Updated function object to return lyric

--- backend/services/gptService.js
+++ backend/services/gptService.js
@@ -14,10 +14,14 @@ const tools = [
         type: "function",
         function: {
             name: "createSongObject",
-            description: "Given the user's input create a song object with the name of the song, artist, and link to its lyrics on genius.com.",
+            description: "Given the user's input create a query object with the requested lyric, the name of the song, artist, and link to its lyrics on genius.com.",
             parameters: {
                 type: "object",
                 properties: {
+                    lyric: {
+                        type:"string",
+                        description: "The specified lyric the user is looking for"
+                    },
                     name: {
                         type: "string",
                         description: "The english or romanized name of the song"
@@ -31,7 +35,7 @@ const tools = [
                         description: "The link to the song lyrics on genius.com"
                     },
                 },
-                required: ["name", "artist", "geniusLink"]
+                required: ["lyric","name", "artist", "geniusLink"]
             }
         }
     }
@@ -45,14 +49,15 @@ const messages = [
     },
     {
         role: "user",
-        content: "Can you give me the information about the song called Metro Spider by Metro Boomin?"
+        content: "Can you tell me the number of times 'Adele' is said in Metro Spider by Metro Boomin?"
     },
 ];
 
 const response = await openAIClient.chat.completions.create({
     model: "gpt-4o-mini-2024-07-18",
     messages: messages,
+    tool_choice: "required",
     tools: tools,
 })
-
-console.log(response.choices[0].message.tool_calls?.[0].function)
+let json = JSON.stringify(response.choices[0].message.tool_calls?.[0].function.arguments)
+console.log(json)
