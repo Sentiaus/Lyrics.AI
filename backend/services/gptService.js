588accef442193d7b00a554f1431015e86df4fd9
Victor Fawole <victorfawole@gmail.com>
2024-08-26 17:08:30 -0400

    Wrapped main input method into middleware
    Created a method named readInput that serves as the main middleware function for reading user input.
    Also fixed bug with gpt messages, where the keys have quotes, making it unparseable for other methods.

--- backend/services/gptService.js
+++ backend/services/gptService.js
@@ -1,7 +1,7 @@
 import express from "express"
 import OpenAI from "openai"
 import dotenv from 'dotenv'
-dotenv.config({path:'../.env'})
+dotenv.config()
 
 // Need to make sure this works when deployed
 const openAIClient = new OpenAI({
@@ -22,7 +22,7 @@ const tools = [
                         type:"string",
                         description: "The specified lyric the user is looking for"
                     },
-                    name: {
+                    title: {
                         type: "string",
                         description: "The english or romanized name of the song"
                     },
@@ -35,7 +35,7 @@ const tools = [
                         description: "The link to the song lyrics on genius.com"
                     },
                 },
-                required: ["lyric","name", "artist", "geniusLink"]
+                required: ["lyric","title", "artist", "geniusLink"]
             }
         }
     }
@@ -47,17 +47,46 @@ const messages = [
         role: "system", 
         content: "You are a helpful language parsing assistant. Use the supplied tools to assist the user."
     },
-    {
-        role: "user",
-        content: "Can you tell me the number of times 'Adele' is said in Metro Spider by Metro Boomin?"
-    },
 ];
 
-const response = await openAIClient.chat.completions.create({
-    model: "gpt-4o-mini-2024-07-18",
-    messages: messages,
-    tool_choice: "required",
-    tools: tools,
-})
-let json = JSON.stringify(response.choices[0].message.tool_calls?.[0].function.arguments)
-console.log(json)
+
+
+const readInput = async (req,res, next) => {
+    const openAIClient = new OpenAI({
+        apiKey: process.env['OPENAI_API_KEY']
+    })
+    // Get content from request
+    const {content} = req.body
+
+    // Validate input
+    if(!content){
+        return res.status(400).json({error: "Please input a query"})
+    }
+
+    // Append users message to the conversation
+    messages.push({role: "user", content})
+
+    // Get response from GPT4o-mini model
+    const response = await openAIClient.chat.completions.create({
+        model: "gpt-4o-mini-2024-07-18",
+        messages: messages,
+        tool_choice: "required", // Must always return function
+        tools: tools,
+    })
+
+    // Get function response
+    const jsonOutput = response.choices[0].message.tool_calls?.[0].function.arguments
+
+    // Push tool output to messages
+    if (jsonOutput){
+        messages.push({role:"tools", content: JSON.stringify(jsonOutput)})
+    }
+
+    // Modify and return request with parsed output in the shape of {lyric, name, artist, geniusLink}
+    req.body = JSON.parse(jsonOutput)
+    next()
+}
+
+
+export { readInput }
+
