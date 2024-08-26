aeadbdbe8443f91714f27f567630366ac9c26982
Victor Fawole <victorfawole@gmail.com>
2024-08-26 17:10:47 -0400

    Added gptService readInput as middleware to post
    readInput should now work as middleware for any functions and will
    specifically return a json of {lyric, title, artist, geniusLink}

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { addSong, getSongs, scrapeSong } from "../controllers/songController.js"
+import { readInput } from "../services/gptService.js"
 
 const router = express.Router()
 
@@ -7,7 +8,7 @@ const router = express.Router()
 router.get('/', getSongs)
 
 /******* Add one Song *******/
-router.post('/', addSong)
+router.post('/', readInput, addSong)
 
 // Testing
 router.get('/scrape', scrapeSong)
