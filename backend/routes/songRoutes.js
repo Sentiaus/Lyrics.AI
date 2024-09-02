5610bae594bf4cfb864080036043c01c18384ba9
Victor Fawole <victorfawole@gmail.com>
2024-09-02 13:23:42 -0400

    Removed middleware from songRoutes function

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addSong, getSongs } from "../controllers/songController.js"
+import { addSong, getSongs, searchSong } from "../controllers/songController.js"
 import { readInput } from "../services/gptService.js"
 
 const router = express.Router()
@@ -10,5 +10,6 @@ router.get('/', getSongs)
 /******* Add one Song *******/
 router.post('/', readInput, addSong)
 
+router.get('/search', searchSong)
 
 export { router as songRoutes }
