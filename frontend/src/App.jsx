12156035332b98f8148f06f26a58660f576b2633
Victor Fawole <victorfawole@gmail.com>
2024-09-09 19:54:23 -0400

    Created Form for Main Page
    Much work to do!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,31 @@
-import { useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  // Form state data
+  const [formData, setFormData] = useState({
+    query: ''
+  });
+
+  // Handle query
+  const handleQuery = (e) => {
+    e.preventDefault();
+    console.log(formData.query)
+  }
   return (
     <div className="container">
       <div className="header">
         <h1>Lyric.AI</h1>
       </div>
       <div className="formcontainer">
-        <form className="formItself">
+        <form className="formItself" onSubmit={handleQuery}>
         <textarea
             name="query"
             placeholder="How many times does Adele say 'Hello' in Hello?"
             className=""
             rows="4"
+            value= {formData.query}
+            onChange={(e)=>setFormData({...formData, query: e.target.value})}
           />
           <button
             type="submit"
