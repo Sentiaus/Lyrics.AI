b66867ed5176ebfc65e7197ffac6ce2d7e55b0c0
Victor <fawole.5@osu.edu>
2024-09-08 20:58:47 -0400

    Deleted Tailwind Stylings and fixed form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // Form Data
-  
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold mb-8">Lyrics.AI</h1>
-      <div className="w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
-        <form className="flex flex-col">
-          <textarea
+    <div className="container">
+      <div className="header">
+        <h1>Lyric.AI</h1>
+      </div>
+      <div className="formcontainer">
+        <form className="formItself">
+        <textarea
             name="query"
             placeholder="How many times does Adele say 'Hello' in Hello?"
-            className="resize-none rounded-md border border-gray-300 p-4 h-32 focus:outline-none focus:border-blue-500"
+            className=""
             rows="4"
           />
           <button
             type="submit"
-            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            className="formsubmit"
           >
             Submit
           </button>
